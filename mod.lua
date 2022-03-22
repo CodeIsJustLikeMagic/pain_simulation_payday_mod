7cd43bbe7bc395c7913ae26b06b2f611dace0d62
CodeIsJustLikeMagic <janelle.pfeifer@freenet.de>
2022-03-22 16:24:39 +0100

    superblt hppt get request,  it works, omg how nice is it

--- mod.lua
+++ mod.lua
@@ -1,9 +1,7 @@
 
 Hooks:PostHook(HUDTeammate, "init", "init_health_as_number_standalone", function(self, ...)
-    local host, port = "127.0.0.1", 8001
-    local socket = require("socket")
-    local tcp = assert(socket.tcp())
-    tcp:connect(host,port);
-    tcp:send("I have the high ground")
-    tcp:close()
+    log("hello there")
+    dohttpreq("http://localhost:8001/ihavethehighground", function(data)
+        log("answer "+data)
+    end)
 end)
