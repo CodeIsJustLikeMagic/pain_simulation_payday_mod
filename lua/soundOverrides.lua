95ef37e212d996aa6accb579036026a0b6d1c4ea
CodeIsJustLikeMagic <janelle.pfeifer@freenet.de>
2022-05-31 23:22:33 +0200

    run sound on damage

--- lua/soundOverrides.lua
+++ lua/soundOverrides.lua
@@ -1,7 +1,7 @@
 -- sound changes
 local ThisModPath = ModPath
 local snd_path = ThisModPath .. "assets/duckquack.ogg"
-local snd_path2 = ThisModPath .. "assets/obi-wan-hello-there.ogg"
+local snd_path2 = ThisModPath .. "assets/obi-wan-hello-there-adjusted.ogg"
 
 if not io.file_is_readable(snd_path) then
     log("painevent ERROR cannot find filepath " .. snd_path)
@@ -14,23 +14,27 @@ else
     log("painevent ERROR stopping mod !!!")
 end
 
-local mod_ids = Idstring(ThisModPath):key()
-local hook1 = "F"..Idstring("hook1::"..mod_ids):key()
-local hook2 = "F"..Idstring("hook2::"..mod_ids):key()
-local msgr1 = "F"..Idstring("msgr1::"..mod_ids):key()
 
-Hooks:PostHook(PlayerDamage, "init", hook1, function(self)
+Hooks:PostHook(PlayerDamage, "init", "init_pain_event", function(self)
     log("painevent playerdamage init")
-    managers.player:unregister_message(Message.OnPlayerDodge, msgr1)
-    managers.player:register_message(Message.OnPlayerDodge, msgr1, function()
+    managers.player:unregister_message(Message.OnPlayerDodge, "onDodge_pain_event")
+    managers.player:register_message(Message.OnPlayerDodge, "onDodge_pain_event", function()
         log("painevent running my sound")
         XAudio.UnitSource:new(XAudio.PLAYER, XAudio.Buffer:new(snd_path)):set_volume(1)
         log("painevent running my sound done")
     end)
+
+    managers.player:unregister_message(Message.OnPlayerDamage, "onDamage_pain_event")
+    managers.player:register_message(Message.OnPlayerDamage, "onDamage_pain_event", function()
+        log("painevent running my sound")
+        XAudio.UnitSource:new(XAudio.PLAYER, XAudio.Buffer:new(snd_path2)):set_volume(1)
+        log("painevent running my sound done")
+    end)
+
     log("painevent registered Message that plays sound on dodge soundpath: " .. snd_path)
 end)
 
-Hooks:PreHook(PlayerDamage, "pre_destroy", hook2, function(self)
+Hooks:PreHook(PlayerDamage, "pre_destroy", "pre_destory_pain_event", function(self)
     log("painevent playerdamage pre destroy")
-    managers.player:unregister_message(Message.OnPlayerDodge, msgr1)
+    managers.player:unregister_message(Message.OnPlayerDodge, "onDodge_pain_event")
 end)
