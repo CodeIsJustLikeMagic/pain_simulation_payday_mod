cb8ed7023d938cb4225c511a6793cb66937f35f4
CodeIsJustLikeMagic <janelle.pfeifer@freenet.de>
2022-07-20 17:00:02 +0200

    disabled default damage sound effects

--- lua/visual_auditory_effects.lua
+++ lua/visual_auditory_effects.lua
@@ -218,6 +218,31 @@ if string.lower(RequiredScript) == "lib/managers/hudmanager" then
     end)
 end
 
-Hooks:PostHook(PlayerSound, "play", "play_pain_event", function(self, sound_name, source_name, sync)
-    log("painevent playersound "..sound_name)
-end)
+--replace whole function. needs required script so PlayerSound isnt a nil value
+if string.lower(RequiredScript) == "lib/units/beings/player/playersound" then
+    function PlayerSound:play(sound_name, source_name, sync)
+        log("painevent playersound "..sound_name)
+
+        --disable hit sounds
+        if sound_name ~= "player_hit" and sound_name ~= "player_hit_permadamage" and sound_name ~= "player_armor_gone_stinger" then
+            local event_id = nil
+
+            if type(sound_name) == "number" then
+                event_id = sound_name
+                sound_name = nil
+            end
+
+            if sync then
+                event_id = event_id or SoundDevice:string_to_id(sound_name)
+                source_name = source_name or ""
+
+                self._unit:network():send("unit_sound_play", event_id, source_name)
+            end
+
+            local event = self:_play(sound_name or event_id, source_name)
+
+            return event
+        end
+
+    end
+end
